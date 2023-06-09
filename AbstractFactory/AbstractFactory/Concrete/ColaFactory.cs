7072e7e68462fcc75649d5b4529b0a9ad6c404f9
Farid <101619655+faridibirov@users.noreply.github.com>
2023-06-10 03:38:28 +0400

    Add files via upload

---
+++ AbstractFactory/AbstractFactory/Concrete/ColaFactory.cs
@@ -0,0 +1,24 @@
+﻿using AbstractFactory.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Concrete
+{
+    internal class ColaFactory : ISoftDrinksFactory
+    {
+
+
+        public IClassic ProduceClassic() => new ClassicCola();
+
+        public IFruit ProduceFruit() => new Fanta();
+
+
+        public ISoda ProduceSoda() => new Bonaqua();
+
+        public IZero ProduceZero() => new ColaZero();
+     
+    }
+}
