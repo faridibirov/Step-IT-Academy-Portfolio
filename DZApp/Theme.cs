9b1d39bbbcce00a766846a55748c5cb292eb760d
Farid <101619655+faridibirov@users.noreply.github.com>
2023-06-10 02:31:10 +0400

    Add files via upload

---
+++ DZApp/Theme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DZApp;
+
+public partial class Theme
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Book> Books { get; } = new List<Book>();
+
+    public override string ToString()
+    {
+        return $"{Name}";
+    }
+}
