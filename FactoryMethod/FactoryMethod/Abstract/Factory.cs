03af255e80eb3fd4f09c79ab45c6cc5a07fcbaef
Farid <101619655+faridibirov@users.noreply.github.com>
2023-06-10 04:32:05 +0400

    Add files via upload

---
+++ FactoryMethod/FactoryMethod/Abstract/Factory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Abstract
+{
+    abstract class Factory
+    {
+        public void Produce()
+        {
+            Console.WriteLine("Produce product");
+        }
+        public abstract IDepartment ProduceProduct();
+
+    }
+}
