f84dd6be2f705c129f1ea934dab6c309dfecaea1
Дибиров Фарид Абдуллаевич <Dibi_yq66@itstep.edu.az>
2023-09-25 09:59:45 +0400

    adding provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
