8ceee5af6de37f19754c060530239f740bb4c8cf
Дибиров Фарид Абдуллаевич <Dibi_yq66@itstep.edu.az>
2023-10-02 12:14:46 +0400

    Init react app

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+console.log("hello world");
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Paragraph from './Components/Paragraph/Paragraph';
+function App()
+{
+    return(
+       <Paragraph text={"5"}/>
+    )
+}
+
+export default App;
